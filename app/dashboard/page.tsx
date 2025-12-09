50a423e69d55c038b09d8b8f996e23ccc5c637c2
jtn11 <jatinsainii007@gmail.com>
2025-12-09 23:00:07 +0530

    logout route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,21 @@
+import { useRouter } from "next/navigation";
+
 export default function Dashboard(){
+
+
+    const router = useRouter();
+
+    async function handleLogout (){
+        await fetch("/api/auth/logout" , {
+            method : "POST"
+        })
+        router.push("/signin")
+    }
     return (
         <div>
             <h1>Dashboard Page</h1>
+            <br />
+            <button onClick={handleLogout}>Logout </button>
         </div>
     )
 }
